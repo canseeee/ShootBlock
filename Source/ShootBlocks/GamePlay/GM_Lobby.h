ecf061d1f64e5c2d06c6e8db580ec38a9f501e95
wei <128288781+canseeee@users.noreply.github.com>
2024-12-02 22:15:00 +0800

    Add files via upload

---
+++ Source/ShootBlocks/GamePlay/GM_Lobby.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PC_Lobby.h"
+#include "GM_Lobby.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOOTBLOCKS_API AGM_Lobby : public AGameModeBase
+{
+	GENERATED_BODY()
+public:
+	AGM_Lobby();
+
+};
---
+++ Source/ShootBlocks/ShootBlocksGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShootBlocksGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AShootBlocksGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AShootBlocksGameMode();
+};
+
+
+
